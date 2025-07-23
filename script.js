8bed5d30b5ba93a3df4ed35b7b7c053d056b0c4d
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-23 20:38:57 +0530

    Make a 16 by 16 grid using JS create side input button and make a hard boundary for sketch-box

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const side = prompt("Enter the side of Square gird");
+let side =16;
+const button=document.querySelector("#inputButton");
+console.log(button);
+button.addEventListener("click",inputButtonEventHandler);
+
+function inputButtonEventHandler (){
+    side = prompt("Enter the required side size of square gird",16);
+    if (side>100)
+        prompt("Side size out of range enter value than 100",16);
+    console.log("inside input handler")
+    }
+
+const gridContainer = document.querySelector("#container");
+
+ for(i=0;i<side;i++){
+    let gridPixelV = document.createElement("div");
+    gridPixelV.style.height='20px';
+    gridPixelV.classList.add('pixel');
+    gridContainer.appendChild(gridPixelV);
+    for(j=0;j<side;j++){
+        let gridPixelH = document.createElement("div");
+        gridPixelH.style.height='20px';
+        gridPixelH.style.border='2px solid black';
+        gridPixelH.classList.add('pixel');
+        gridPixelV.appendChild(gridPixelH);
+
+
+    }
+
+ }
+    
