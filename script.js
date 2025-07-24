c6bf8c68d13ccfb08865da3f6aa3ceb42d942ae1
Shivank Yadav <194476296+shivankyadav2022@users.noreply.github.com>
2025-07-24 09:53:36 +0530

    Introduce the hover functionality remove background color and borders of pixels

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let side =8;
+let side =16;
+
+// a button with input handler that change the side of grid as per user choice//
 const button=document.querySelector("#inputButton");
-console.log(button);
 button.addEventListener("click",inputButtonEventHandler);
 
 function inputButtonEventHandler (){
     side = prompt("Enter the required side size of square gird",16);
     if (side>100)
         prompt("Side size out of range enter value than 100",16);
-    console.log("inside input handler")
+    // Remove the Initial Grid//
+    const elementsToRemove = document.querySelectorAll(".pixel");
+    elementsToRemove.forEach(element=>element.remove());
+
+    //Make a new grid based on new side size//
+    constructGrid();
+
+    // add hover event listener to paint pixels//
+
+const elementsToColor = document.querySelectorAll(".pixel");
+
+elementsToColor.forEach(elements => {
+    elements.addEventListener("mouseover",()=>{
+        elements.style.backgroundColor="black";
+    })
+})
     }
 
 const gridContainer = document.querySelector("#container");
-
+//function that constructs a grid of empty div items//
+function constructGrid (){
  for(i=0;i<side;i++){
     let gridPixelV = document.createElement("div");
     const dimension = (400/side);
@@ -21,7 +38,7 @@ const gridContainer = document.querySelector("#container");
         let gridPixelH = document.createElement("div");
         gridPixelH.style.height=`${dimension}px`;
         gridPixelH.style.width=`${dimension}px`;
-        gridPixelH.style.border='2px solid black';
+       // gridPixelH.style.border='2px solid black';
         gridPixelH.classList.add('pixel');
         gridPixelV.appendChild(gridPixelH);
 
@@ -29,4 +46,16 @@ const gridContainer = document.querySelector("#container");
     }
 
  }
-    
+}
+// Construct the default grid at the time of loading of page//
+constructGrid();
+
+// add hover event listener to paint pixels//
+
+const elementsToColor = document.querySelectorAll(".pixel");
+
+elementsToColor.forEach(elements => {
+    elements.addEventListener("mouseover",()=>{
+        elements.style.backgroundColor="black";
+    })
+})
